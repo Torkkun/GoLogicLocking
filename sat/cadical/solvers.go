a9c626da3cdca1f95eb45c84fa9e4e2105735cef
Torkkun <onigirikaribian@gmail.com>
2024-12-11 12:20:54 +0900

    base commit

--- sat/cadical/solvers.go
+++ sat/cadical/solvers.go
@@ -1,6 +1,5 @@
 package cadical
 
-/*
 func NewSolver() {
 	// Cadical 2.1.0のみ
 
@@ -10,13 +9,9 @@ type Cadical210 struct {
 }
 
 func NewCadical210() {
-	if not
+
 }
 
 func (*Cadical210) Delete() {
 
 }
-
-
-
-*/
